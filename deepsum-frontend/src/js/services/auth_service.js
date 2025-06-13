1d671048dcb04de801169688ef8d5cc3b8b9aafa
Zaidan R <zidan1217@gmail.com>
2025-06-13 17:03:35 +0700

    fix callback #2

--- deepsum-frontend/src/js/services/auth_service.js
+++ deepsum-frontend/src/js/services/auth_service.js
@@ -8,10 +8,7 @@ async function signInWithProvider(provider) {
       throw new Error("Provider not supported");
     }
 
-    const redirectTo =
-      process.env.NODE_ENV === "production"
-        ? `${window.location.origin}/auth_callback.html`
-        : `https://deepsum.vercel.app/auth/callback`;
+    const redirectTo = `https://deepsum.vercel.app/auth/callback`;
 
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider,
