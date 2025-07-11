bba934288cdf0fdf26fb6ef6169bc2c974660ee6
Zaidan R <zidan1217@gmail.com>
2025-07-11 10:21:41 +0700

    change api url #8

--- deepsum-frontend/src/js/services/api_service.js
+++ deepsum-frontend/src/js/services/api_service.js
@@ -1,8 +1,9 @@
 import { supabase } from "../supabase_client";
 
 // const API_BASE_URL = "https://deepsum.space";
-const API_BASE_URL = "https://51df-114-10-46-45.ngrok-free.app";
+// const API_BASE_URL = "https://51df-114-10-46-45.ngrok-free.app";
 // const API_BASE_URL = "https://89c4-182-253-124-146.ngrok-free.app";
+const API_BASE_URL = "https://ce900fb0fb60.ngrok-free.app;";
 
 async function getCurrentUser() {
   const {
